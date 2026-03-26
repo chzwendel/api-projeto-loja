ce9138403c06ba4b5ee2ba5d9ecf05ef6ccbdb2c
chzwendel <christianwendell@hotmail.com>
2026-03-25 21:26:14 -0300

    atualização TUDO novamente

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,13 +1,119 @@
-import Database from "better-sqlite3";
+import database from "better-sqlite3";
 import path from "path";
-import fs from "fs";
 
-const dbPath = path.resolve(__dirname, "../../database.db");
-const schemaPath = path.resolve(__dirname, "./schema.sql");
+const dbPath = path.resolve(__dirname, "../../banco.db");
+const db = new database(dbPath);
 
-const db = new Database(dbPath);
+db.pragma("foreign_keys = ON");
 
-const schema = fs.readFileSync(schemaPath, "utf-8");
-db.exec(schema);
+db.exec(`
+  CREATE TABLE IF NOT EXISTS Produto (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    tamanho VARCHAR(50) NOT NULL,
+    cor VARCHAR(50) NOT NULL,
+    codigo_barras VARCHAR(50) NOT NULL UNIQUE,
+    marca VARCHAR(50) NOT NULL,
+    valor_custo DECIMAL(10, 2) NOT NULL,
+    valor_venda DECIMAL(10, 2) NOT NULL,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    quantidade INT NOT NULL,
+    id_estoque INT NOT NULL,
+    id_fornecedor INT NOT NULL,
+    id_usuarios INT NOT NULL,
+    FOREIGN KEY (id_estoque) REFERENCES estoque(id),
+    FOREIGN KEY (id_fornecedor) REFERENCES fornecedor(id),
+    FOREIGN KEY (id_usuarios) REFERENCES usuarios(id)
+);
+
+CREATE TABLE IF NOT EXISTS Fornecedor(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_empresarial VARCHAR(100) NOT NULL,
+    cnpj VARCHAR(20) NOT NULL UNIQUE,
+    endereco VARCHAR(100) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    forma_pagamento VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Pedido (
+    numero_pedido INTEGER PRIMARY KEY AUTOINCREMENT,
+    forma_pagamento VARCHAR(100) NOT NULL,
+    status_pagamento VARCHAR(100) NOT NULL,
+    status_pedido VARCHAR(100) NOT NULL,
+    id_usuarios INT NOT NULL,
+    valor_total DECIMAL(10,2) NOT NULL,
+    data\_pedido DATETIME NOT NULL DEFAULT CURRENT\_TIMESTAMP,
+    quantidade INT NOT NULL,
+    FOREIGN KEY (id_usuarios) REFERENCES usuarios(id)
+);
+
+CREATE TABLE IF NOT EXISTS PedidoItem (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_pedido INT NOT NULL,
+    id_produto INT NOT NULL,
+    quantidade INT NOT NULL,
+    FOREIGN KEY (id\_pedido) REFERENCES pedido(numero\_pedido),
+    FOREIGN KEY (id_produto) REFERENCES produtos(id)
+);
+
+CREATE TABLE IF NOT EXISTS Usuario (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    telefone VARCHAR(15) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha TEXT NOT NULL,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS Funcionario (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   nome VARCHAR(100) NOT NULL,
+   cpf VARCHAR(11) NOT NULL UNIQUE,
+   data_nascimento DATE NOT NULL,
+   senha TEXT NOT NULL,
+   cargo VARCHAR(15) NOT NULL,
+   nivel_permissao
+
+CREATE TABLE IF NOT EXISTS Compra (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cep VARCHAR(8) NOT NULL,
+    numero_casa VARCHAR(10) NOT NULL,
+    frete DECIMAL(10, 2) NOT NULL,
+    forma_pagamento VARCHAR(100) NOT NULL,
+    quantidade INT NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS Avaliacao (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    curtida VARCHAR(1) NOT NULL,
+    estrelas INT NOT NULL,
+    comentario VARCHAR(255),
+    id_usuarios INT NOT NULL,
+    FOREIGN KEY (id_usuarios) REFERENCES usuarios(id)
+);
+
+CREATE TABLE IF NOT EXISTS Estoque(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    entrada_mercadoria DATE NOT NULL,
+    status_produto VARCHAR(100) NOT NULL,
+    quantidade_produto INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Categoria (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    beneficios VARCHAR(100),
+    preco DECIMAL(10,2),
+    id_categoria INT,
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id)
+);
+`);
 
 export default db;
--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,97 +1,107 @@
-CREATE TABLE if NOT EXISTS produtos (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-nome VARCHAR(100) NOT NULL,
-categoria VARCHAR(50) NOT NULL,
-tamanho VARCHAR(50) NOT NULL,
-cor VARCHAR(50) NOT NULL,
-codigo_barras VARCHAR(50) NOT NULL UNIQUE,
-marca VARCHAR(50) NOT NULL,
-valor_custo DECIMAL(10, 2) NOT NULL,
-valor_venda DECIMAL(10, 2) NOT NULL,
-createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-quantidade INT NOT NULL,
-id_estoque INT NOT NULL,
-id_fornecedor INT NOT NULL,
-id_usuarios INT NOT NULL,
-FOREIGN KEY (id_estoque) REFERENCES estoque(id),
-FOREIGN KEY (id_fornecedor) REFERENCES fornecedor(id),
-FOREIGN KEY (id_usuarios) REFERENCES usuarios(id)
+CREATE TABLE IF NOT EXISTS Produto (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    tamanho VARCHAR(50) NOT NULL,
+    cor VARCHAR(50) NOT NULL,
+    codigo_barras VARCHAR(50) NOT NULL UNIQUE,
+    marca VARCHAR(50) NOT NULL,
+    valor_custo DECIMAL(10, 2) NOT NULL,
+    valor_venda DECIMAL(10, 2) NOT NULL,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    quantidade INT NOT NULL,
+    id_estoque INT NOT NULL,
+    id_fornecedor INT NOT NULL,
+    id_usuarios INT NOT NULL,
+    FOREIGN KEY (id_estoque) REFERENCES estoque(id),
+    FOREIGN KEY (id_fornecedor) REFERENCES fornecedor(id),
+    FOREIGN KEY (id_usuarios) REFERENCES usuarios(id)
 );
 
-CREATE TABLE if NOT EXISTS fornecedor(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-nome_empresarial VARCHAR(100) NOT NULL,
-cnpj VARCHAR(20) NOT NULL UNIQUE,
-endereco VARCHAR(100) NOT NULL,
-telefone VARCHAR(15) NOT NULL,
-forma_pagamento VARCHAR(100) NOT NULL
+CREATE TABLE IF NOT EXISTS Fornecedor(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_empresarial VARCHAR(100) NOT NULL,
+    cnpj VARCHAR(20) NOT NULL UNIQUE,
+    endereco VARCHAR(100) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    forma_pagamento VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE if NOT EXISTS pedido (
-numero_pedido INTEGER PRIMARY KEY AUTOINCREMENT,
-forma_pagamento VARCHAR(100) NOT NULL,
-status_pagamento VARCHAR(100) NOT NULL,
-status_pedido VARCHAR(100) NOT NULL,
-id_usuarios INT NOT NULL,
-valor_total DECIMAL(10,2) NOT NULL,
-data_pedido DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-quantidade INT NOT NULL,
-FOREIGN KEY (id_usuarios) REFERENCES usuarios(id)
+CREATE TABLE IF NOT EXISTS Pedido (
+    numero_pedido INTEGER PRIMARY KEY AUTOINCREMENT,
+    forma_pagamento VARCHAR(100) NOT NULL,
+    status_pagamento VARCHAR(100) NOT NULL,
+    status_pedido VARCHAR(100) NOT NULL,
+    id_usuarios INT NOT NULL,
+    valor_total DECIMAL(10,2) NOT NULL,
+    data\_pedido DATETIME NOT NULL DEFAULT CURRENT\_TIMESTAMP,
+    quantidade INT NOT NULL,
+    FOREIGN KEY (id_usuarios) REFERENCES usuarios(id)
 );
 
-CREATE TABLE if NOT EXISTS pedido_item (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-id_pedido INT NOT NULL,
-id_produto INT NOT NULL,
-quantidade INT NOT NULL,
-FOREIGN KEY (id_pedido) REFERENCES pedido(numero_pedido),
-FOREIGN KEY (id_produto) REFERENCES produtos(id)
+CREATE TABLE IF NOT EXISTS PedidoItem (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_pedido INT NOT NULL,
+    id_produto INT NOT NULL,
+    quantidade INT NOT NULL,
+    FOREIGN KEY (id\_pedido) REFERENCES pedido(numero\_pedido),
+    FOREIGN KEY (id_produto) REFERENCES produtos(id)
 );
 
-CREATE TABLE if NOT EXISTS usuarios (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-nome VARCHAR(100) NOT NULL,
-cpf VARCHAR(11) NOT NULL UNIQUE,
-telefone VARCHAR(15) NOT NULL,
-data_nascimento DATE NOT NULL,
-endereco VARCHAR(100) NOT NULL,
-email VARCHAR(100) NOT NULL UNIQUE,
-senha TEXT NOT NULL,
-createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS Usuario (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    telefone VARCHAR(15) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha TEXT NOT NULL,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE if NOT EXISTS compras (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-cep VARCHAR(8) NOT NULL,
-numero_casa VARCHAR(10) NOT NULL,
-frete DECIMAL(10, 2) NOT NULL,
-forma_pagamento VARCHAR(100) NOT NULL,
-quantidade INT NOT NULL,
-status VARCHAR(50) NOT NULL,
-createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS Funcionario (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   nome VARCHAR(100) NOT NULL,
+   cpf VARCHAR(11) NOT NULL UNIQUE,
+   data_nascimento DATE NOT NULL,
+   senha TEXT NOT NULL,
+   cargo VARCHAR(15) NOT NULL,
+   nivel_permissao
+
+CREATE TABLE IF NOT EXISTS Compra (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cep VARCHAR(8) NOT NULL,
+    numero_casa VARCHAR(10) NOT NULL,
+    frete DECIMAL(10, 2) NOT NULL,
+    forma_pagamento VARCHAR(100) NOT NULL,
+    quantidade INT NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE if NOT EXISTS avaliacao (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-curtida VARCHAR(1) NOT NULL,
-estrelas INT NOT NULL,
-comentario VARCHAR(255),
-id_usuarios INT NOT NULL,
-FOREIGN KEY (id_usuarios) REFERENCES usuarios(id)
+CREATE TABLE IF NOT EXISTS Avaliacao (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    curtida VARCHAR(1) NOT NULL,
+    estrelas INT NOT NULL,
+    comentario VARCHAR(255),
+    id_usuarios INT NOT NULL,
+    FOREIGN KEY (id_usuarios) REFERENCES usuarios(id)
 );
 
-CREATE TABLE if NOT EXISTS estoque(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-entrada_mercadoria DATE NOT NULL,
-status_produto VARCHAR(100) NOT NULL,
-quantidade_produto INT NOT NULL
+CREATE TABLE IF NOT EXISTS Estoque(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    entrada_mercadoria DATE NOT NULL,
+    status_produto VARCHAR(100) NOT NULL,
+    quantidade_produto INT NOT NULL
 );
 
-CREATE TABLE if NOT EXISTS categoria (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-nome VARCHAR(100) NOT NULL,
-beneficios VARCHAR(100),
-preco DECIMAL(10,2),
-id_categoria INT,
-FOREIGN KEY (id_categoria) REFERENCES categoria(id)
+CREATE TABLE IF NOT EXISTS Categoria (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    beneficios VARCHAR(100),
+    preco DECIMAL(10,2),
+    id_categoria INT,
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id)
 );
